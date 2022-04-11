f128148da95e64b15cbd31ad405d43ae2c35979f
Ravinder yadav <ravinder.y@crowdbotics.com>
2022-04-11 14:09:57 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -8,7 +8,24 @@ const Untitled2 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      </View>;
+      <View style={{
+      left: 74,
+      top: 41,
+      position: "absolute",
+      width: 80,
+      height: 80,
+      backgroundColor: "#E4E4E4",
+      borderRadius: "50%"
+    }}></View><View style={{
+      left: 25,
+      top: 195,
+      position: "absolute",
+      height: 60,
+      width: 140,
+      backgroundColor: "#E4E4E4",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View></View>;
 };
 
 export default Untitled2;
